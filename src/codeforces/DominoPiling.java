8937359d8a7c3c3209b2589d67c729282cae9725
hasib <hasnain23105341116@diu.edu.bd>
2024-09-20 13:45:14 +0600

    DominoPiling Problem has been solved

---
+++ src/codeforces/DominoPiling.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class DominoPiling {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int m = input.nextInt();
+        int n = input.nextInt();
+        System.out.print((m*n)/2);
+    }
+
+}
